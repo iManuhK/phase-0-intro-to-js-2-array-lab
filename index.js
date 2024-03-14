28acfc2f8c71b6a9038639c4d029d364df01f5bc
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-03-14 12:45:00 +0300

    Modifies intro to functions lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+function destructivelyAppendCat(name) {
+    destructivelyAppendCat = cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    destructivelyPrependCat = cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    destructivelyRemoveLastCat = cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    appendCat = [...cats ,"Broom"];
+    return appendCat ;
+}
+function prependCat(name) {
+    prependCat = ["Arnold",...cats];
+    return prependCat;
+}
+function removeLastCat() {
+    removeLastCat =  cats.slice(0,cats.length-1);
+    return removeLastCat;
+}
+function removeFirstCat() {
+    removeFirstCat = cats.slice(1);
+    return removeFirstCat;
+}
